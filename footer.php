3d1b9f9bf6109bec0ec44a1e6f4625a66766d5a4
trịnh sỹ dự <du_trinhsy@yahoo.com>
2020-08-18 20:36:31 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
         <footer class="bg-white bounce animated sticky-footer">
             <div class="container my-auto">
-                <div class="text-center my-auto copyright"><span>nhóm 1 ưdpm4</span></div>
+                <div class="text-center my-auto copyright"><span>nhóm 1 ưdpm4 <a href="https://github.com/kuuyby9duoi/Nhom1_UDPM4_CodeGym" src="mã nguồn" target="_blank">Source code</a></span></div>
             </div>
         </footer>
     </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
